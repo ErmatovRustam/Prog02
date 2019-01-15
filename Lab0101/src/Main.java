d0ba06aaa0cd1e0b53d16139651fc353067f17df
Rustam Ermatov <ermatov_r@au.kg>
2019-01-15 12:09:53 +0600

    Lab0101: Adding methods

--- Lab0101/src/Main.java
+++ Lab0101/src/Main.java
@@ -3,13 +3,54 @@ public class Main {
     static final int H = 20;
     static final int W = 20;
     static char[][] field = new char[H][W];
+    static boolean penDown = false;
+    static final int turtleRow = 0;
+    static final int turtleCol = 0;
+    static final int UP = 0;
+    static final int RIGHT = 1;
+    static final int DOWN = 2;
+    static final int LEFT = 3;
+    static int turtleDir = RIGHT;
+
 
     public static void main(String[] args) {
 
 
         init();
+        penDown();
+        move(10);
+        turnRight();
+        move(10);
+        turnRight();
+        move(10);
+        turnRight();
+        move(10);
+        turnRight();
         print();
     }
+
+    private static void turnRight() {
+
+        
+    }
+
+    private static void move(int step) {
+        for (int i = 0; i < step; ++i) {
+            ++turtleDir;
+            if (turtleDir == 4) {
+                turtleDir = UP;
+            }
+
+        }
+        if (penDown) {
+            for (int i = 0; i < 10; ++i) {
+                for (int j = 0; j < 10; ++j) {
+                    field[i][j] = '*';
+                }
+            }
+        }
+    }
+
 
     static void init() {
 
@@ -18,6 +59,10 @@ public class Main {
                 field[i][j] = '.';
             }
         }
+    }
+
+    private static void penDown() {
+        penDown = true;
     }
 
     static void print() {
